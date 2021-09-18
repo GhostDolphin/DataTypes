d038c79a11954ae55f82004cdc8cb907b70e7206
GhostDolphin <d.ghost2ac@gmail.com>
2021-09-18 21:47:33 +0300

    DataTypes ALL taska

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (arr) => {
+  const counted = {};
+  for (const element of arr) {
+    const type = typeof element;
+    if (counted[type] === undefined) counted[type] = 0;
+    counted[type]++;
+  }
+  return counted;
+};
 
 module.exports = { countTypesInArray };
